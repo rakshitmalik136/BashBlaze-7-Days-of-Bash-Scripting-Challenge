99376c01e5f8d0cffeec571c42df8c950615cd1f
Rakshit Malik <rakshit.malik136@gmail.com>
2025-02-17 22:36:12 +0530

    Done with day_1 tasks

---
+++ Challenges/Day_1/day_1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This was the first task assigned to me.
+
+#Task 2
+echo "Hello! I am Rakshit"
+
+#Task 3
+variable1="hello"
+variable2="dosto"
+echo "$variable1 $variable2"
+
+#Task 4
+greetings="$variable1 $variable2"
+echo "$greetings, welcome to bash script."
+
+#Task 5
+echo "My current bash path - $BASH"
+echo "Bash version I am using - $BASH_VERSION"
+echo "PID of bash I am running - $$"
+echo "My home directory - $HOME"
+echo "Where am I currently? - $PWD"
+echo "My hostname - $HOSTNAME"
+
+#Task 6
+echo "Files with .txt extension in the current directory:"
+ls *.txt
+
